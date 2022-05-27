34071a9f32258c5fba15facd62eaf51004e32974
Asif Khan <asifk@chetu.com>
2022-05-27 23:00:24 +0530

    Creating navigation bar

--- src/App.js
+++ src/App.js
@@ -1,60 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
-import Directory from "./components/directory/directory.component";
+
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return <div>I am the shop component</div>;
+};
 
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-    {
-      id: 7,
-      title: "dogs",
-      imageUrl:
-        "https://www.adobe.com/express/feature/image/media_16ad2258cac6171d66942b13b8cd4839f0b6be6f3.png?width=750&format=png&optimize=medium",
-    },
-    {
-      id: 8,
-      title: "leopards",
-      imageUrl:
-        "https://freepngimg.com/thumb/leopard/5-2-leopard-download-png.png",
-    },
-    {
-      id: 9,
-      title: "sarees",
-      imageUrl:
-        "https://files.oyebesmartest.com/uploads/large/indian-girl-png-transpatg6qf.png",
-    },
-    {
-      id: 10,
-      title: "flowers",
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPJBgTPwloSonY7MdOitXhm-Yz95pQ4apqwA&usqp=CAU",
-    },
-  ];
-
-  return <Directory categories={{ categories }} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import { ReactComponent as ReactLogo } from "../../assets/crown.svg";
+import "./navigation.style.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <ReactLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link to="/shop" className="nav-link">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
